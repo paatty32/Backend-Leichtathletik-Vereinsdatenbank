a64ff16a198afa2fde046df948b3a04c55836cfe
patty32 <patrick.boadu.boafo@hotmail.de>
2024-02-01 00:10:21 +0100

    Refactor - Creating Modules for the layers

--- backend-leichtathletik-vereinsdatenbank/leichtathletik-vereinsdatenbank-controller/src/main/java/de/boadu/leichtathletik/vereinsdatenbank/LeichtathletikVereinsdatenbankApplication.java
+++ backend-leichtathletik-vereinsdatenbank/leichtathletik-vereinsdatenbank-controller/src/main/java/de/boadu/leichtathletik/vereinsdatenbank/LeichtathletikVereinsdatenbankApplication.java
@@ -1,9 +1,14 @@
-package de.boadu.backend.leichtathletik.vereinsdatenbank;
+package de.boadu.leichtathletik.vereinsdatenbank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
+@PropertySources({
+		@PropertySource("classpath:dao-application.properties")
+})
 public class LeichtathletikVereinsdatenbankApplication {
 
 	public static void main(String[] args) {
