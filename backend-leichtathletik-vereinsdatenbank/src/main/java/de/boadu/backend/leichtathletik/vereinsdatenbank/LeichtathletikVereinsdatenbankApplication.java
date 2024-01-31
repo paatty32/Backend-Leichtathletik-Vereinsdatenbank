5a5e5bf0463981b8cd9f956192c82887f2bc2830
patty32 <patrick.boadu.boafo@hotmail.de>
2024-01-31 23:36:53 +0100

    initial project

---
+++ backend-leichtathletik-vereinsdatenbank/src/main/java/de/boadu/backend/leichtathletik/vereinsdatenbank/LeichtathletikVereinsdatenbankApplication.java
@@ -0,0 +1,13 @@
+package de.boadu.backend.leichtathletik.vereinsdatenbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeichtathletikVereinsdatenbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeichtathletikVereinsdatenbankApplication.class, args);
+	}
+
+}
---
+++ backend-leichtathletik-vereinsdatenbank/src/test/java/de/boadu/backend/leichtathletik/vereinsdatenbank/LeichtathletikVereinsdatenbankApplicationTests.java
@@ -0,0 +1,13 @@
+package de.boadu.backend.leichtathletik.vereinsdatenbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeichtathletikVereinsdatenbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
