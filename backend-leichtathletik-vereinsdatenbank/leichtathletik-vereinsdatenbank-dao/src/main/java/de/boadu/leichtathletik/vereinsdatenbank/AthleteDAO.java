2a7957805b9771b3d68008b931e2b0af3d444575
patty32 <patrick.boadu.boafo@hotmail.de>
2024-02-01 00:10:56 +0100

    DAO ADD - add first entity and application.properties file

---
+++ backend-leichtathletik-vereinsdatenbank/leichtathletik-vereinsdatenbank-dao/src/main/java/de/boadu/leichtathletik/vereinsdatenbank/AthleteDAO.java
@@ -0,0 +1,27 @@
+package de.boadu.leichtathletik.vereinsdatenbank;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name="Athlete")
+public class AthleteDAO {
+
+    @Id
+    @Column(name="startpassnummer")
+    public int startpassnummer;
+
+    @Column(name="nachname")
+    public String surname;
+
+    @Column(name="vorname")
+    public String name;
+
+    @Column(name="jahrgang")
+    public int yearOfBirth;
+
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "startpassnummer" )
+    public List<CompetitionResultsDAO> competitionResults;
+
+}
---
+++ backend-leichtathletik-vereinsdatenbank/leichtathletik-vereinsdatenbank-dao/src/main/java/de/boadu/leichtathletik/vereinsdatenbank/CompetitionResultsDAO.java
@@ -0,0 +1,46 @@
+package de.boadu.leichtathletik.vereinsdatenbank;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.math.BigInteger;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name="wettkampfergebnis")
+public class CompetitionResultsDAO {
+
+    @Id
+    private BigInteger id;
+
+    @Column(name="datum")
+    private Timestamp date;
+
+    @Column(name="ort")
+    private String place;
+
+    @Column(name="disziplin")
+    private String dicipline;
+
+    @Column(name="ergebnis")
+    private String result;
+
+    @Column(name="halle")
+    private boolean isIndoor;
+
+    @Column(name="wind")
+    private String wind;
+
+    @Column(name="kommentar")
+    private String comment;
+
+    @Column(name="startpassnummer")
+    private int startpassnummer;
+
+    @Override
+    public String toString() {
+        return this.result + " " + this.place;
+    }
+}
