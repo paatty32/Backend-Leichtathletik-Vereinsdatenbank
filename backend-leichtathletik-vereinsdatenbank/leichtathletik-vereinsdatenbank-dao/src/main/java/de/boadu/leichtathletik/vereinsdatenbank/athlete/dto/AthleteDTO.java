1d1bd5c0dbf21b996ce3330c580ef18ff11482c4
patty32 <patrick.boadu.boafo@hotmail.de>
2024-02-02 00:05:05 +0100

    DAO ADD - create dto and repository for Domain athlete

---
+++ backend-leichtathletik-vereinsdatenbank/leichtathletik-vereinsdatenbank-dao/src/main/java/de/boadu/leichtathletik/vereinsdatenbank/athlete/dto/AthleteDTO.java
@@ -0,0 +1,13 @@
+package de.boadu.leichtathletik.vereinsdatenbank.athlete.dto;
+
+public interface AthleteDTO {
+
+    int getStartpassnummer();
+
+    String getName();
+
+    String getSurname();
+
+    int getYearOfBirth();
+
+}
