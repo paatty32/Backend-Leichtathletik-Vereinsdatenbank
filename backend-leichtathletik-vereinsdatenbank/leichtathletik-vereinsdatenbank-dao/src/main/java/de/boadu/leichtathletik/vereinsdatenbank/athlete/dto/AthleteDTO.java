1c365c2aa6ef7c1d1f9b028fbb0a3e1b04dc67e6
patrickb <patrick.boadu.boafo@hotmail.de>
2024-02-02 17:27:29 +0100

    DAO REFACTORING - changing DTO Interface to Record

--- backend-leichtathletik-vereinsdatenbank/leichtathletik-vereinsdatenbank-dao/src/main/java/de/boadu/leichtathletik/vereinsdatenbank/athlete/dto/AthleteDTO.java
+++ backend-leichtathletik-vereinsdatenbank/leichtathletik-vereinsdatenbank-dao/src/main/java/de/boadu/leichtathletik/vereinsdatenbank/athlete/dto/AthleteDTO.java
@@ -1,13 +1,5 @@
 package de.boadu.leichtathletik.vereinsdatenbank.athlete.dto;
 
-public interface AthleteDTO {
-
-    int getStartpassnummer();
-
-    String getName();
-
-    String getSurname();
-
-    int getYearOfBirth();
+public record AthleteDTO (int startpassnummer, String name, String surname, int yearOfBirth){
 
 }
